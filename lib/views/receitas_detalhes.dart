053b756860c88ff6462b23d265b5e04abb01ddaa
deividinacio <deivid_@outlook.com>
2024-10-17 14:42:53 -0300

    implementação tela favoritos, add icon favorito e controller favorito

--- lib/views/receitas_detalhes.dart
+++ lib/views/receitas_detalhes.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:loja_flutter/constants/app_styles.dart';
+import 'package:loja_flutter/controller/favoritos_controller.dart';
+//import 'package:loja_flutter/controller/receitas_controller.dart';
 import 'package:loja_flutter/models/receita_model.dart';
 
 class ReceitasDetalhes extends StatefulWidget {
@@ -16,6 +18,8 @@ class ReceitasDetalhes extends StatefulWidget {
   State<ReceitasDetalhes> createState() => _ReceitasDetalhesState();
 }
 
+bool isFavorite = false;
+
 class _ReceitasDetalhesState extends State<ReceitasDetalhes> {
   @override
   Widget build(BuildContext context) {
@@ -28,15 +32,34 @@ class _ReceitasDetalhesState extends State<ReceitasDetalhes> {
 
         body: SingleChildScrollView(
           child: Column(children: [
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20.0),
-                child: Image.network(widget.receita.strMealThumb, 
-                width: 300,
+            Stack(
+              children: [
+              Image.network(widget.receita.strMealThumb, 
+              height: 300,
+              width: double.infinity,
+              ),
+
+              Positioned(
+                right: 30, // posicionamento do coração para favorito
+                child: IconButton(
+                  onPressed: (){
+
+                    //  além de mudar a cor do icone, a lista de favoritos deve ser populada
+                    FavoritosController().addToFavoriteList(widget.receita);
+                    setState(() {
+                      isFavorite = !isFavorite;
+                    });
+                  },
+                  icon: isFavorite ? Icon(Icons.favorite, 
+                  color:  Color.fromARGB(255, 255, 0, 0)) : 
+                  Icon(Icons.favorite_outline_outlined,
+                  color: Color.fromARGB(255, 0, 0, 0),
+                  ), 
                 ),
               ),
+            ]
             ),
+          
             Padding(
               padding: const EdgeInsets.all(24.0),
               child: Text(widget.receita.strMeal,
@@ -55,7 +78,7 @@ class _ReceitasDetalhesState extends State<ReceitasDetalhes> {
               textAlign: TextAlign.justify,
               ),
             ),
-
+          
             Text('Ingrediente : ${widget.receita.strIngredient1}'),
           ],
           ),
--- lib/views/receitas_favoritas.dart
+++ lib/views/receitas_favoritas.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:loja_flutter/controller/favoritos_controller.dart';
 
 class ReceitasFavoritas extends StatelessWidget {
   const ReceitasFavoritas({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    print(FavoritosController().allFavoritos().length);
     return  Scaffold(
       appBar: AppBar(
       title: const Text("Favoritos"),
       backgroundColor: Colors.grey,
     ),
-    body: const Column(
-      children: [
-        Text("Favoritos aqui"),
-        Text("Favoritos aqui 2"),
-      ],
-    ),
+    body: ListView.builder(
+    padding: const EdgeInsets.all(8),
+    itemCount: 20,
+    itemBuilder: (BuildContext context, int index) {
+      return const ListTile(
+          leading: FlutterLogo(),
+          title:  Text("Favoritos aqui"),
+          trailing: Icon(Icons.arrow_forward_ios_rounded),
+        );
+    }),
     );
   }
 }
